ce02a55aa40985f9f5abe92a17c44fba39c8fbe3
srmooon <srmoon_@outlook.com>
2024-11-30 17:48:20 -0500

    Fix deployment configuration

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   images: {
-    domains: ['res.cloudinary.com']
+    domains: ['res.cloudinary.com'],
+    unoptimized: true
   },
   typescript: {
     ignoreBuildErrors: true
